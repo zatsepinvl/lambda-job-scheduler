4d4d8a12bffe5d4b2c9cae06ed1f025551329c97
Vladimir Zatsepin <vladimir.zatsepin@dataart.com>
2023-02-17 21:56:11 +0100

    Add IntervalTriggerTest

--- luartz/src/main/kotlin/org/luartz/trigger/IntervalTrigger.kt
+++ luartz/src/main/kotlin/org/luartz/trigger/IntervalTrigger.kt
@@ -1,18 +1,24 @@
 package org.luartz.trigger
 
+import java.time.Clock
 import java.time.Duration
 import java.time.Instant
 
 class IntervalTrigger(
+    val clock: Clock = Clock.systemDefaultZone(),
     val startAt: Instant,
     val interval: Duration
 ) : Trigger {
     override fun mayFireAgain() = true
 
     override fun nextFireTime(): Instant {
-        val now = Instant.now()
-        val k: Int = ((now.toEpochMilli() - startAt.toEpochMilli()) / interval.toMillis()).toInt()
-        return startAt.plusMillis(interval.toMillis() * (k + 1))
+        val now = clock.instant()
+        return if (now.isBefore(startAt)) {
+            startAt.plusMillis(interval.toMillis())
+        } else {
+            val k: Int = ((now.toEpochMilli() - startAt.toEpochMilli()) / interval.toMillis()).toInt()
+            startAt.plusMillis(interval.toMillis() * (k + 1))
+        }
     }
 
     override fun updateAfterFired() {}
---
+++ luartz/src/test/kotlin/org/luartz/trigger/IntervalTriggerTest.kt
@@ -0,0 +1,62 @@
+package org.luartz.trigger
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+import java.time.Clock
+import java.time.Duration
+import java.time.Instant
+import java.time.ZoneId
+
+class IntervalTriggerTest {
+
+    @Test
+    fun testFireTimeBeforeStartTime() {
+        // Given
+        val clock = givenFixedClock(0)
+        val trigger = givenIntervalTrigger(clock, 10, 1)
+
+        // When
+        val fireTime = trigger.nextFireTime()
+
+        // Then
+        assertThat(fireTime.toEpochMilli()).isEqualTo(11)
+    }
+
+    @Test
+    fun testFireTimeAfterStartTime() {
+        // Given
+        val clock = givenFixedClock(10)
+        val trigger = givenIntervalTrigger(clock, 10, 1)
+
+        // When
+        val fireTime = trigger.nextFireTime()
+
+        // Then
+        assertThat(fireTime.toEpochMilli()).isEqualTo(11)
+    }
+
+    @Test
+    fun testFireTimeAfterAWhile() {
+        // Given
+        val clock = givenFixedClock(22)
+        val trigger = givenIntervalTrigger(clock, 10, 5)
+
+        // When
+        val fireTime = trigger.nextFireTime()
+
+        // Then
+        assertThat(fireTime.toEpochMilli()).isEqualTo(25)
+    }
+
+    private fun givenFixedClock(timeMillis: Long): Clock {
+        return Clock.fixed(Instant.ofEpochMilli(timeMillis), ZoneId.systemDefault())
+    }
+
+    private fun givenIntervalTrigger(clock: Clock, startAtMillis: Long, intervalMillis: Long): IntervalTrigger {
+        return IntervalTrigger(
+            clock,
+            Instant.ofEpochMilli(startAtMillis),
+            Duration.ofMillis(intervalMillis)
+        )
+    }
+}

29e36d51ccd7f7951b116619d441aff5b474aede
Vladimir Zatsepin <vladimir.zatsepin@dataart.com>
2023-02-21 14:39:37 +0100

    Scheduler smoke tests

--- luartz/src/test/kotlin/scheduler/SchedulerImplTest.kt
+++ luartz/src/test/kotlin/scheduler/SchedulerImplTest.kt
@@ -1,14 +1,17 @@
 package scheduler
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.luartz.executor.JobExecutor
 import org.luartz.job.Job
 import org.luartz.job.JobDefinition
 import org.luartz.job.JobState
 import org.luartz.scheduler.JobScheduleRequest
+import org.luartz.scheduler.Scheduler
 import org.luartz.scheduler.SchedulerImpl
 import org.luartz.store.MutableJobStore
 import org.luartz.trigger.NowTrigger
+import org.luartz.trigger.Trigger
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import org.mockito.kotlin.any
@@ -18,35 +21,64 @@ import java.lang.Thread.sleep
 
 class SchedulerImplTest {
 
-    @Test
-    fun smokeTest() {
-        // Given
-        val executor: JobExecutor = mock()
-        val store: MutableJobStore = mock()
+    private lateinit var executor: JobExecutor
+    private lateinit var store: MutableJobStore
+    private lateinit var scheduler: Scheduler
 
-        whenever(executor.execute(any())).thenAnswer {
+    @BeforeEach
+    fun setup() {
+        executor = mock()
+        store = mock()
+        scheduler = SchedulerImpl(executor, store)
+    }
+
+    @Test
+    fun executorIsInvoked() {
+        // Given
+        executor.mockSuccessInvocation()
+        val request = givenTestJobScheduleRequest()
+
+        // When
+        scheduler.whenScheduledAndAwaited(request)
+
+        // Then
+        verify(executor, times(1)).execute(any())
+    }
+
+    @Test
+    fun storeIsInvoked() {
+        // Given
+        executor.mockSuccessInvocation()
+        val request = givenTestJobScheduleRequest()
+
+        // When
+        scheduler.whenScheduledAndAwaited(request)
+
+        // Save on scheduled, run and then executed
+        verify(store, times(3)).save(any())
+    }
+
+    private fun givenTestJobScheduleRequest(trigger: Trigger = NowTrigger()): JobScheduleRequest {
+        return JobScheduleRequest(
+            name = "test_name",
+            definition = JobDefinition("test", "test"),
+            payload = "test_payload",
+            trigger = trigger
+        )
+    }
+
+    private fun JobExecutor.mockSuccessInvocation() {
+        whenever(this.execute(any())).thenAnswer {
             val job = it.arguments[0] as Job
             job.state = JobState.SUCCEEDED
             job
         }
-        val scheduler = SchedulerImpl(executor, store)
+    }
 
-        // When
-        scheduler.schedule(
-            JobScheduleRequest(
-                name = "test_name",
-                definition = JobDefinition("test", "test"),
-                payload = "test_payload",
-                trigger = NowTrigger()
-            )
-        )
-        scheduler.start()
-        sleep(100)
-        scheduler.shutdown()
-
-        // Then
-        verify(executor, times(1)).execute(any())
-        // Save on submitted and then executed
-        verify(store, times(2)).save(any())
+    private fun Scheduler.whenScheduledAndAwaited(request: JobScheduleRequest, awaitMillis: Long = 200) {
+        this.schedule(request)
+        this.start()
+        sleep(awaitMillis)
+        this.shutdown()
     }
 }

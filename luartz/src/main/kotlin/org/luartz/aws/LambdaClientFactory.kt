f4924086156d7e45cd9492e914fb6230bfa65ea3
Vladimir Zatsepin <vladimir.zatsepin@dataart.com>
2023-02-24 22:31:51 +0100

    Fix typo

--- luartz/src/main/kotlin/org/luartz/aws/LambdaClientFactory.kt
+++ luartz/src/main/kotlin/org/luartz/aws/LambdaClientFactory.kt
@@ -3,11 +3,11 @@ package org.luartz.aws
 import software.amazon.awssdk.services.lambda.LambdaClient
 import java.net.URI
 
-private const val AWS_ENDPOINT_ENV = "AWS_ENDPOINT_URL"
+private const val AWS_ENDPOINT_URL = "AWS_ENDPOINT_URL"
 
 object LambdaClientFactory {
     fun create(): LambdaClient {
-        val endpointUrl = System.getenv(AWS_ENDPOINT_ENV)
+        val endpointUrl = System.getenv(AWS_ENDPOINT_URL)
         return if (endpointUrl != null) {
             LambdaClient.builder()
                 .endpointOverride(URI.create(endpointUrl))

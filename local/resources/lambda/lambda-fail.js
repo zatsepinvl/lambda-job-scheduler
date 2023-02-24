d3517291af3f127c7ca7dfcd621f624150cffe59
Vladimir Zatsepin <vladimir.zatsepin@dataart.com>
2023-02-24 22:35:00 +0100

    Rename local to localstack; Remove unused resource;

--- local/resources/lambda/lambda-fail.js
+++
@@ -1,3 +0,0 @@
-exports.handler = async function (event) {
-    throw new Error("Test error on event: " + JSON.stringify(event));
-}
--- local/resources/lambda/lambda-success.js
+++
@@ -1,4 +0,0 @@
-exports.handler = async function (event) {
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
-    return event;
-}

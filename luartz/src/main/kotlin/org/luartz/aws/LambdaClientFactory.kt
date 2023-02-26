dd3608f78701961213d98bccf741fddef3b0763c
Vladimir Zatsepin <vladimir.zatsepin@dataart.com>
2023-02-26 11:40:35 +0100

    Refactor SCheduler fabric

--- luartz/src/main/kotlin/org/luartz/aws/LambdaClientFactory.kt
+++ luartz/src/main/kotlin/org/luartz/aws/LambdaClientFactory.kt
@@ -3,11 +3,11 @@ package org.luartz.aws
 import software.amazon.awssdk.services.lambda.LambdaAsyncClient
 import java.net.URI
 
-private const val AWS_ENDPOINT_URL = "AWS_ENDPOINT_URL"
+private const val AWS_LAMBDA_ENDPOINT_URL = "AWS_LAMBDA_ENDPOINT_URL"
 
 object LambdaClientFactory {
     fun createAsync(): LambdaAsyncClient {
-        val endpointUrl = System.getenv(AWS_ENDPOINT_URL)
+        val endpointUrl = System.getenv(AWS_LAMBDA_ENDPOINT_URL)
         return if (endpointUrl != null) {
             LambdaAsyncClient.builder()
                 .endpointOverride(URI.create(endpointUrl))
--- luartz/src/main/kotlin/org/luartz/scheduler/SchedulerFabric.kt
+++ luartz/src/main/kotlin/org/luartz/scheduler/SchedulerFabric.kt
@@ -5,19 +5,23 @@ import org.luartz.executor.JobExecutor
 import org.luartz.executor.LambdaJobExecutor
 import org.luartz.store.InMemoryJobStore
 import org.luartz.store.MutableJobStore
+import software.amazon.awssdk.services.lambda.LambdaAsyncClient
 
 object SchedulerFabric {
 
-    fun create(store: MutableJobStore, executor: JobExecutor): Scheduler {
-        return SchedulerImpl(executor, store)
+    fun createDefault(): Scheduler {
+        val lambdaClient = LambdaClientFactory.createAsync()
+        return createDefaultWithLambdaClient(lambdaClient)
     }
 
-    fun createDefault(): Scheduler {
+    fun createDefaultWithLambdaClient(lambdaAsyncClient: LambdaAsyncClient): Scheduler {
         val store = InMemoryJobStore()
-
-        val lambdaClient = LambdaClientFactory.createAsync()
-        val executor = LambdaJobExecutor(lambdaClient)
+        val executor = LambdaJobExecutor(lambdaAsyncClient)
 
         return create(store, executor)
+    }
+
+    fun create(store: MutableJobStore, executor: JobExecutor): Scheduler {
+        return SchedulerImpl(executor, store)
     }
 }

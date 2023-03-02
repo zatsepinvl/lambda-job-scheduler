a830a30fd0a17b41097521f16456e4e31177c2f3
Vladimir Zatsepin <vladimir.zatsepin@dataart.com>
2023-03-02 11:47:53 +0100

    Add java doc

--- luartz/src/main/kotlin/org/luartz/scheduler/Scheduler.kt
+++ luartz/src/main/kotlin/org/luartz/scheduler/Scheduler.kt
@@ -3,15 +3,33 @@ package org.luartz.scheduler
 import org.luartz.store.JobStore
 
 interface Scheduler {
+    /**
+     * Schedule a job for execution. Do not take any effect until #start is invoked.
+     */
     fun schedule(template: JobTemplate)
 
+    /**
+     * Unschedule a job from execution. Do not affect already running jobs.
+     */
     fun unschedule(templateId: String)
 
+    /**
+     * Start scheduling and executing jobs. Do not block current thread.
+     */
     fun start()
 
+    /**
+     * Shutdown scheduler. Might block current thread until all internal resources are terminated.
+     */
     fun shutdown()
 
+    /**
+     * Returns instance of currently using job store.
+     */
     fun getJobStore(): JobStore
 
+    /**
+     * Get the list of all currently scheduled job templates.
+     */
     fun getJobTemplates(): List<JobTemplate>
 }
